7694277d0a1c1a48522f7d05e2454c02c7b0b30b
Francis_Fernandez <francisfernandez1011@gmail.com>
2023-07-12 01:36:00 +0530

    Error resolved headerItem

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,7 @@ HiStar,
 HiPlayCircle,
 HiTv } from "react-icons/hi2";
 import { HiPlus, HiDotsVertical } from "react-icons/hi";
-import HeaderItem from './Headeritem';
+import HeaderItem from './HeaderItem';
 
 
 function Header() {
